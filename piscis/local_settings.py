a7e510a43bf0e9d75a9044c37c59b882745d7366
geralfaro <55108904+geralfaro@users.noreply.github.com>
2022-09-13 11:31:58 -0300

    Add files via upload

---
+++ piscis/local_settings.py
@@ -0,0 +1,7 @@
+LOGIN_REDIRECT_URL = "index"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "piscis.proyect@gmail.com"
+EMAIL_HOST_PASSWORD = "piscis1234"
+EMAIL_PORT = 587
